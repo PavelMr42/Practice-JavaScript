6db30159256404f609291abebd2fa95a0d8a70e1
PavelMr42 <Pashagrigorev403@gmail.com>
2023-03-27 17:07:44 +0700

    movie subset app

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-const answer = prompt('Вам есть 18 лет?', 'да');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMoveBD = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
+
+personalMoveBD.movies[a] = b;
+personalMoveBD.movies[c] = d;
+
+console.log(personalMoveBD);
