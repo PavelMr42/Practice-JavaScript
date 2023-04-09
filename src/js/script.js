f0ffdb3153d444a9121ffbf44f62b527dd011f81
PavelMr42 <Pashagrigorev403@gmail.com>
2023-04-09 12:27:55 +0700

    adding the trim() method

--- src/js/script.js
+++ src/js/script.js
@@ -21,8 +21,8 @@ const personalMoveBD = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = +prompt('На сколько оцените его?', '');
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+              b = +prompt('На сколько оцените его?', '');
 
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
             personalMoveBD.movies[a] = b;
